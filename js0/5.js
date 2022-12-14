6554bc45d37b11c16e311a5170a0a88d261c7ac2
Rick Mays <8709540+rickmays@users.noreply.github.com>
2022-12-14 10:36:29 -0600

    solves js0 problem 5

--- js0/5.js
+++ js0/5.js
@@ -8,9 +8,15 @@
  */
 
 const solution = (num1, num2, num3) => {
-  return 0
-}
+  if (num1 > num2 && num1 > num3) {
+    return num1;
+  } else if (num2 > num1 && num2 > num3) {
+    return num2;
+  } else {
+    return num3;
+  }
+};
 
 module.exports = {
-  solution
-}
+  solution,
+};
